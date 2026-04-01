0a7f4fcf0d35861615cb8e3cccc8494f9be11c1a
socils <ajuliuszch@gmail.com>
2026-04-01 16:51:23 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,5 @@
-// 1. Usamos la URL completa para que el navegador pueda descargar Firebase
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
-import { getAuth, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
-
-// Tus credenciales reales de EduSolve
-const firebaseConfig = {
+// firebase-config.js
+var firebaseConfig = {
   apiKey: "AIzaSyDffVQzP6yaiUX4SsE3ghoBBBGyr7thMFs",
   authDomain: "edusolve-947c1.firebaseapp.com",
   projectId: "edusolve-947c1",
@@ -12,9 +8,8 @@ const firebaseConfig = {
   appId: "1:1089784819674:web:5f0e310b0a8ae699191ae3"
 };
 
-// 2. Inicializamos la conexión
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);   
-
-// 3. Exportamos las funciones para usarlas en login.html e index.html
-export { onAuthStateChanged, signOut };         
+// ESTA ES LA LÍNEA QUE FALTA O NO SE ESTÁ EJECUTANDO:
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+    console.log("✅ Alonixz-Cloud: App Inicializada");
+}
